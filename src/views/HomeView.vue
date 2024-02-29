f5144f86dccfd8233c613bff43e19ee1e58e864f
Xin <161011294+liuxin0207@users.noreply.github.com>
2024-02-29 10:30:54 +0800

    go

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+
+    methods: {
+
+
+
+    }
+  }
+}
+
+
+
+</script>
+
+<template>
+  <div flex flex-col>
+    <div class="flex flex-col gap-y-2">
+      <h1 class="text-8xl font-bold"></h1>
+    </div>
+</div>
+<div class="flex">
+  <div class="">編號</div>
+  <div>姓名</div>
+  <div>身份證</div>
+  <div>性別</div>
+  <div>電話</div>
+  <div>功能</div>
+</div>
+</template>
+
+<style scoped></style>
